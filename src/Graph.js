3845f15e20901c903f1fe6efe61c9d6abfafdfc0
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-12 02:32:58 -0500

    Update Graph functionality with node storage and result printing
    - Moved `prettyprint` function outside of the `Graph` class for reusability.
    - Added `this.nodes` to the `Graph` class to track the sequence of edges/nodes.
    - Updated `addEdge` to store `node.data` in `this.nodes`.
    - Implemented `printResult` to display the knight's traversal path, number of moves, and start/end positions.

--- src/Graph.js
+++ src/Graph.js
@@ -6,10 +6,12 @@ export class Graph {
     for (let i = 0; i < size; i++) {
       this.matrix[i] = new Array(size).fill(0);
     }
+    this.nodes = [];
   }
 
-  addEdge(row, col) {
-    this.matrix[row][col] = 1;
+  addEdge(node) {
+    this.matrix[node.row][node.col] = 1;
+    this.nodes.push(node.data);
   }
 
   checkEdge(row, col) {
@@ -20,20 +22,35 @@ export class Graph {
     }
   }
 
-  prettyprint() {
-    let header = 'i   ';
-    for (let j = 0; j < this.matrix.length; j++) {
-      header += j + ' ';
-    }
-    console.log(header);
-    console.log();
+  printResult() {
+    console.log(
+      `=> KnightMoves([${this.nodes[0]}], [${this.nodes[this.nodes.length - 1]}])`,
+    );
 
-    for (let i = 0; i < this.matrix.length; i++) {
-      let row = i + '   ';
-      for (let j = 0; j < this.matrix[i].length; j++) {
-        row += this.matrix[i][j] + ' ';
-      }
-      console.log(row.trim());
+    console.log(
+      `=> You made it in ${this.nodes.length - 1} ${this.nodes.length - 1 === 1 ? 'move' : 'moves'}! Here's your path:`,
+    );
+
+    for (let node of this.nodes) {
+      console.log(node);
     }
   }
+}
+
+export function prettyprint(matrix) {
+  let header = 'i   ';
+  for (let j = 0; j < matrix.length; j++) {
+    header += j + ' ';
+  }
+  console.log(header);
+  console.log();
+
+  for (let i = 0; i < matrix.length; i++) {
+    let row = i + '   ';
+    for (let j = 0; j < matrix[i].length; j++) {
+      row += matrix[i][j] + ' ';
+    }
+    console.log(row.trim());
+  }
+  console.log();
 }
